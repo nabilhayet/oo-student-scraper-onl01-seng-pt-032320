edd6150e04c63ee2669a8a4263537cf294a1d666
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-05 04:47:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    doc.css(".roster-cards-container .student-card").each do |profile|
+    student ={}
+    array=[]
+    student[:name] = profile.css(".student-name").text
+    student[:location]=profile.css(".student-location").text
+    student[:profile_url]=profile.attr('href a')
+    array << student
   end
+end 
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
