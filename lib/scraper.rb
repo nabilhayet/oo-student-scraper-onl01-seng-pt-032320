e841478272b406ed64baa3e774a653d121b955ab
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-10 01:59:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -45,7 +45,7 @@ end
     profile[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
 
     profile
-  end 
+  end
 
     def self.movie
       html = open("https://www.afi.com/afis-100-years-100-movies/")
@@ -55,3 +55,4 @@ end
 
     end
 end
+end 
