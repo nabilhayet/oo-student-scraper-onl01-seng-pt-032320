4fc29a0111198c6ea5279cd8b5bc4b0adea8661c
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-10 02:02:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -45,9 +45,9 @@ end
     profile[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
 
     profile
-    html = open("https://www.afi.com/afis-100-years-100-movies/")
-    doc = Nokogiri::HTML(html)
-    Post = doc.css(".row")
+    html1 = open("https://www.afi.com/afis-100-years-100-movies/")
+    doc1 = Nokogiri::HTML(html1)
+    Post = doc1.css(".row")
     binding.pry
   end
 end
